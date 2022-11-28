06fd803b32475dc5cc3addd4176e8d048c8e2378
hoang <nguyenxuanhoang710@gmail.com>
2022-11-28 08:33:11 +0700

    Edit file SecurityConfig

---
+++ taco-cloud-api/src/main/java/xhoang/api/IngredientController.java
@@ -0,0 +1,38 @@
+package xhoang.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import xhoang.data.IngredientRepository;
+import xhoang.tacos.Ingredient;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
+public class IngredientController {
+
+    private IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientController(IngredientRepository ingredientRepository){
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public Iterable<Ingredient> allIngredients(){
+        return ingredientRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient){
+        return ingredientRepository.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId){
+        ingredientRepository.deleteById(ingredientId);
+    }
+
+}
